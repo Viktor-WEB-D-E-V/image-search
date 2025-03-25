471503d9e7173323d27ba6df7b06b5bd7495bdff
Viktor Kalyn <viktorkalynwebdev@gmail.com>
2025-03-24 21:54:35 -0900

    Added function to fetch images from Pixabay API

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+const API_KEY = '22970342-054b8243005dd74f99097cd8f';
+const BASIC_URL = 'https://pixabay.com/api/';
+
+function getPixabayImages(value) {
+  return axios
+    .get(
+      `${BASIC_URL}?key=${API_KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true`
+    )
+    .then(response => response.data);
+}
+
+export default {
+  getPixabayImages,
+};
--- src/main.js
+++ src/main.js
@@ -1,2 +1,4 @@
-//import API from './js/pixabay-api';
+import API from './js/pixabay-api';
 //import render from './js/render-function';
+
+API.getPixabayImages('flowers').then(imgsObj => console.log(imgsObj));
