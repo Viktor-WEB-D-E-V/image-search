9f27984f1ea00be42610457f834806602e5a1a99
Viktor Kalyn <viktorkalynwebdev@gmail.com>
2025-03-25 21:12:24 -0900

    Feature: image search by input form

--- src/main.js
+++ src/main.js
@@ -5,45 +5,42 @@ import API from './js/pixabay-api';
 import render from './js/render-function';
 
 const refs = {
-  submitBtn: document.querySelector('.form-btn'),
+  form: document.querySelector('.form'),
 };
 
-refs.submitBtn.addEventListener('click', e => {
+refs.form.addEventListener('submit', e => {
   e.preventDefault();
+  const form = e.currentTarget;
+  const searchQuery = form.elements.search_text.value.trim();
 
-  const testQuery = '';
-
-  console.log(Boolean(testQuery));
-  if (!testQuery) {
-    iziToast.warning({
-      title: 'Warning',
-      message: 'Please enter a value',
-      position: 'topRight',
-    });
-    return;
-  }
-
-  API.getPixabayImages(testQuery)
-    .then(data => {
-      if (data.hits.length === 0) {
-        iziToast.info({
-          title: 'No Results',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-          position: 'topRight',
-        });
-      } else {
-        render.renderGalleryCard(data.hits);
-      }
-    })
-    .catch(() => {
-      iziToast.error({
-        title: 'Error',
-        message: 'Failed to fetch images. Please try again later.',
+    if (!searchQuery) {
+      iziToast.warning({
+        title: 'Warning',
+        message: 'Please enter a value',
         position: 'topRight',
       });
-    });
-});
+      return;
+    }
 
-console.log(refs.submitBtn);
+    API.getPixabayImages(searchQuery)
+      .then(data => {
+        if (data.hits.length === 0) {
+          iziToast.info({
+            title: 'No Results',
+            message:
+              'Sorry, there are no images matching your search query. Please try again!',
+            position: 'topRight',
+          });
+        } else {
+          render.renderGalleryCard(data.hits);
+        }
+      })
+      .catch(() => {
+        iziToast.error({
+          title: 'Error',
+          message: 'Failed to fetch images. Please try again later.',
+          position: 'topRight',
+        });
+      });
+});
 
