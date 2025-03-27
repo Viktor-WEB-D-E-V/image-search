ec582cc7c748c420c5a58cc2f05eb2d74c85f70a
Viktor Kalyn <viktorkalynwebdev@gmail.com>
2025-03-26 19:22:42 -0900

    Feature: Added simplelightbox library

--- src/js/render-function.js
+++ src/js/render-function.js
@@ -1,3 +1,6 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 function renderGalleryCard(images) {
   const gallery = document.querySelector('.gallery');
 
@@ -12,22 +15,34 @@ function renderGalleryCard(images) {
         comments,
         downloads,
       }) => {
-        return `<li class="gallery-item">
-        <a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}">
-        </a>
-        
-        <div class="info">
-          <p class="info-item">Likes: ${likes}</p>
-          <p class="info-item">Comments: ${comments}</p>
-          <p class="info-item">Views: ${views}</p>
-          <p class="info-item">Downloads: ${downloads}</p>
-        </div>
-      </li>`;
+        return `
+        <li class="gallery-item">
+          <a class="gallery-link" href="${largeImageURL}">
+            <img
+              class="gallery-image"
+              src="${webformatURL}"
+              alt="${tags}"
+            />
+            <ul class="gallery-info">
+              <li class="gallery-info-item">Likes <span class="descr-span">${likes}</span></li>
+              <li class="gallery-info-item">Views <span class="descr-span">${views}</span></li>
+              <li class="gallery-info-item">Comments <span class="descr-span">${comments}</span></li>
+              <li class="gallery-info-item">Downloads <span class="descr-span">${downloads}</span></li>
+            </ul> 
+          </a>
+        </li>   
+      `;
       }
     )
     .join('');
-  
+
   gallery.innerHTML = markup;
+
+  const lightbox = new SimpleLightbox('.gallery-item a', {
+    captionDelay: 250,
+    scrollZoom: false,
+  });
+  lightbox.refresh();
 }
 
-export default {renderGalleryCard};
+export default { renderGalleryCard };
